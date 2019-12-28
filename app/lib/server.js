c9ec1b5bd435d275a294d0cf9af5f8e34a5c3240
Shola Ayeni <ayenisholah@yahoo.com>
2019-12-28 08:30:47 +0100

    created new server file to handle server logic

---
+++ app/lib/server.js
@@ -0,0 +1,118 @@
+/**
+ * Server related tasks
+ */
+
+// Dependencies
+var http = require("http");
+var https = require("https");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
+var StringDecoder = require("string_decoder").StringDecoder;
+var config = require("./config");
+var handlers = require("./handlers");
+var helpers = require("./helpers");
+
+var server = {};
+
+server.httpServer = http.createServer((req, res) => {
+  server.unifiedServer(req, res);
+});
+
+server.httpsServerOptions = {
+  key: fs.readFileSync(path.join(__dirname, "/../https/key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "/../https/cert.pem"))
+};
+server.httpsServer = https.createServer(server.httpsServerOptions, (req, res) => {
+  server.unifiedServer(req, res);
+});
+
+// All the server logic for both http and https server
+server.unifiedServer = (req, res) => {
+  // get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Get query string as an object
+  var queryString = parsedUrl.query;
+
+  // Get the HTTP Method
+  var method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  var headers = req.headers;
+
+  // Get the payload if any
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", data => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // Choose the router this request should go to. If route does not exist, use th notFoundHandler
+    var chosenHandler =
+      typeof server.router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // construct data to send to the chosen hander
+    var data = {
+      trimmedPath: trimmedPath,
+      queryString: queryString,
+      method: method,
+      headers: headers,
+      payload: helpers.parseJsonToObject(buffer)
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // use the status code called back by the handler, or default to 200
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+
+      // use the payload called back by the handler, or default to an empty object
+      payload = typeof payload == "object" ? payload : {};
+
+      //convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      // log the response
+      console.log("Returning this response: ", statusCode, payloadString);
+    });
+  });
+};
+
+// Define a request router
+server.router = {
+  ping: handlers.ping,
+  users: handlers.users,
+  tokens: handlers.tokens,
+  checks: handlers.checks
+};
+
+// Initialize script
+server.init = () => {
+  // Start the HTTP server
+  server.httpServer.listen(config.httpPort, () => {
+    console.log(
+      `Magic happening on ${config.httpPort} in ${config.envName} mode`
+    );
+  });
+
+  // Start the HTTPS server
+  server.httpsServer.listen(config.httpsPort, () => {
+    console.log(
+      `Magic happening on ${config.httpsPort} in ${config.envName} mode`
+    );
+  });
+};
+
+module.exports = server;

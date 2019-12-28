cae4f1eb5dd42eca77966bae1bf07b399a9b5ad5
Shola Ayeni <ayenisholah@yahoo.com>
2019-12-28 08:32:29 +0100

    created an helper function to send messages via twilio api

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -5,6 +5,8 @@
 // Dependencies
 var crypto = require("crypto");
 var config = require("./config");
+var https = require("https");
+var querystring = require("querystring");
 
 // Container for helpers
 var helpers = {};
@@ -56,5 +58,71 @@ helpers.createRandomString = strLength => {
     return false;
   }
 };
+
+// Send SMS via Twilio
+helpers.sendTwilioSms = (phone, msg, callback) => {
+  // Validate the parameters
+  phone =
+    typeof phone == "string" && phone.trim().length == 10
+      ? phone.trim()
+      : false;
+
+  msg =
+    typeof msg == "string" && msg.trim().length > 0 && msg.trim().length < 1600
+      ? msg.trim()
+      : false;
+
+  if (phone && msg) {
+    // Configure the request payload
+    var payload = {
+      From: config.twilio.fromPhone,
+      To: `+234${phone}`,
+      Body: msg
+    };
+
+    // Stringify the payload
+    var stringPayload = querystring.stringify(payload);
+
+    // Configure the request details
+    var requestDetails = {
+      protocol: "https:",
+      hostname: "api.twilio.com",
+      method: "POST",
+      path: `/2010-04-01/Accounts${config.twilio.accountSid}/Messages.json`,
+      auth: `${config.twilio.accountSid}:${config.twilio.authToken}`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": Buffer.byteLength(stringPayload)
+      }
+    };
+
+    // Instantiate the request object
+    var req = https.request(requestDetails, res => {
+      // Grab the status of the sent request
+      var status = res.statusCode;
+
+      // Callback successfully if the request went through
+      if (status == 200 || status == 201) {
+        callback(false);
+      } else {
+        callback(`status code returned was ${status}`);
+      }
+    });
+
+    // Bind to the error eventso it doesn't get thrown
+    req.on("error", error => {
+      callback(error);
+    });
+
+    // Add the payload
+    req.write(stringPayload);
+
+    // End the request
+    req.end();
+  } else {
+    callback("Missing or invalid parameter(s)");
+  }
+};
+
 // Export the container
 module.exports = helpers;

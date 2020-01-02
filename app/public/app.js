b8b80525f23f0875b186c29461d213709867b242
Shola Ayeni <ayenisholah@yahoo.com>
2020-01-02 09:06:38 +0100

    created a helper method to get the session token from headers

--- app/public/app.js
+++ app/public/app.js
@@ -192,6 +192,48 @@ app.formResponseProcessor = function(formId, requestPayload, responsePayload) {
   }
 };
 
+// Get the session token from localstorage and set it in the app.config object
+app.getSessionToken = function() {
+  var tokenString = localStorage.getItem("token");
+  if (typeof tokenString == "string") {
+    try {
+      var token = JSON.parse(tokenString);
+      app.config.sessionToken = token;
+      if (typeof token == "object") {
+        app.setLoggedInClass(true);
+      } else {
+        app.setLoggedInClass(false);
+      }
+    } catch (e) {
+      app.config.sessionToken = false;
+      app.setLoggedInClass(false);
+    }
+  }
+};
+
+// Set (or remove) the loggedIn class from the body
+app.setLoggedInClass = function(add) {
+  var target = document.querySelector("body");
+  if (add) {
+    target.classList.add("loggedIn");
+  } else {
+    target.classList.remove("loggedIn");
+  }
+};
+
+// Set the session token in the app.config object as well as localstorage
+app.setSessionToken = function(token) {
+  app.config.sessionToken = token;
+  var tokenString = JSON.stringify(token);
+  localStorage.setItem("token", tokenString);
+  if (typeof token == "object") {
+    app.setLoggedInClass(true);
+  } else {
+    app.setLoggedInClass(false);
+  }
+};
+
+
 // Init (bootstrapping)
 app.init = function() {
   // Bind all form submissions

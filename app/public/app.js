21e657073d4b60770b0fbb9766cc44995047e0e8
Shola Ayeni <ayenisholah@yahoo.com>
2020-01-02 09:07:08 +0100

    created a helper method to renew token

--- app/public/app.js
+++ app/public/app.js
@@ -233,6 +233,58 @@ app.setSessionToken = function(token) {
   }
 };
 
+// Renew the token
+app.renewToken = function(callback) {
+  var currentToken =
+    typeof app.config.sessionToken == "object"
+      ? app.config.sessionToken
+      : false;
+  if (currentToken) {
+    // Update the token with a new expiration
+    var payload = {
+      id: currentToken.id,
+      extend: true
+    };
+    app.client.request(
+      undefined,
+      "api/tokens",
+      "PUT",
+      undefined,
+      payload,
+      function(statusCode, responsePayload) {
+        // Display an error on the form if needed
+        if (statusCode == 200) {
+          // Get the new token details
+          var queryStringObject = { id: currentToken.id };
+          app.client.request(
+            undefined,
+            "api/tokens",
+            "GET",
+            queryStringObject,
+            undefined,
+            function(statusCode, responsePayload) {
+              // Display an error on the form if needed
+              if (statusCode == 200) {
+                app.setSessionToken(responsePayload);
+                callback(false);
+              } else {
+                app.setSessionToken(false);
+                callback(true);
+              }
+            }
+          );
+        } else {
+          app.setSessionToken(false);
+          callback(true);
+        }
+      }
+    );
+  } else {
+    app.setSessionToken(false);
+    callback(true);
+  }
+};
+
 
 // Init (bootstrapping)
 app.init = function() {
